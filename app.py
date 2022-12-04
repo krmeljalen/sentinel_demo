b7c1109e4ae6c138386cdcf8e06ece4b301fdf5d
Alen Krmelj <alen.krmelj@bitstamp.net>
2022-12-04 19:45:17 +0100

    Fixed the app but pipeline creation throws Internal error

--- app.py
+++ app.py
@@ -1,28 +1,9 @@
 #!/usr/bin/env python3
-import os
-
 import aws_cdk as cdk
 
 from sentinel_cdk.sentinel_cdk_stack import SentinelCdkStack
 
-
 app = cdk.App()
-SentinelCdkStack(app, "SentinelCdkStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account="demo", region="eu-west-1"),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+base = SentinelCdkStack(app, "SentinelCdkStack",)
 
 app.synth()
--- sentinel_cdk/sentinel_cdk_stack.py
+++ sentinel_cdk/sentinel_cdk_stack.py
@@ -3,7 +3,10 @@ from aws_cdk import (
     aws_ecr,
     aws_codebuild,
     aws_ec2,
-    CfnOutput, Duration, RemovalPolicy, Stack
+    aws_codepipeline,
+    aws_codepipeline_actions,
+    CfnOutput, Stack, SecretValue,
+    Duration, RemovalPolicy
 )
 from constructs import Construct
 
@@ -20,7 +23,7 @@ class SentinelCdkStack(Stack):
         # ecr repo to push sentinel docker
         ecr = aws_ecr.Repository(
             self, "ECR",
-            repository_name=f"Sentinel",
+            repository_name=f"sentinel",
             removal_policy=RemovalPolicy.DESTROY
         )
 
@@ -28,33 +31,122 @@ class SentinelCdkStack(Stack):
         docker_build = aws_codebuild.PipelineProject(
             self, "DockerBuild",
             project_name=f"Sentinel-Build",
-            build_spec=aws_codebuild.BuildSpec.from_source_filename(
-                filename='sentinel_app/buildspec.yml'),
+            build_spec=aws_codebuild.BuildSpec.from_object({
+                "version": "0.2",
+                "phases": {
+                    "build": {
+                        "commands": [
+                            "$(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)",
+                            "docker build -t $REPOSITORY_URI:latest .",
+                            "docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$CODEBUILD_RESOLVED_SOURCE_VERSION"
+                        ]
+                    },
+                    "post_build": {
+                        "commands": [
+                            "docker push $REPOSITORY_URI:latest",
+                            "docker push $REPOSITORY_URI:$CODEBUILD_RESOLVED_SOURCE_VERSION",
+                            "export imageTag=$CODEBUILD_RESOLVED_SOURCE_VERSION",
+                            "printf '[{\"name\":\"app\",\"imageUri\":\"%s\"}]' $REPOSITORY_URI:$imageTag > imagedefinitions.json"
+                        ]
+                    }
+                },
+                "env": {
+                     "exported-variables": ["imageTag"]
+                },
+                "artifacts": {
+                    "files": "imagedefinitions.json",
+                    "secondary-artifacts": {
+                        "imagedefinitions": {
+                            "files": "imagedefinitions.json",
+                            "name": "imagedefinitions"
+                        }
+                    }
+                }
+            }),
+            environment_variables={
+                "REPOSITORY_URI": aws_codebuild.BuildEnvironmentVariable(
+                    value=ecr.repository_uri
+                )
+            },
             environment=aws_codebuild.BuildEnvironment(
                 privileged=True,
             ),
-            environment_variables={
-                'ecr': aws_codebuild.BuildEnvironmentVariable(
-                    value=ecr.repository_uri),
-                'tag': aws_codebuild.BuildEnvironmentVariable(
-                    value='cdk')
-            },
             description='Pipeline for CodeBuild',
             timeout=Duration.minutes(60),
-        )
-
-        CfnOutput(
-            self, "ECRUrl",
-            description="ECR Url",
-            value=ecr.repository_uri,
         )
 
         # codebuild permissions to push image to ECR
         ecr.grant_pull_push(docker_build)
 
-        # properties for passing to another stack
-        self.output_props['docker_build'] = docker_build
+        # pipeline stuff
+        source_output = aws_codepipeline.Artifact()
 
-    @property
-    def outputs(self):
-        return self.output_props
+        # Create the pipeline
+
+        source_action = aws_codepipeline_actions.GitHubSourceAction(
+            action_name="SourceGithub",
+            owner="alenkrmelj",
+            repo="sentinel_demo",
+            output=source_output,
+            branch="main",
+            oauth_token=SecretValue.secrets_manager("github-oauth-token"),
+        )
+
+        build_action = aws_codepipeline_actions.CodeBuildAction(
+                    action_name="BuildDocker",
+                    project=docker_build,
+                    input=source_output,
+                    outputs=[aws_codepipeline.Artifact("imagedefinitions")],
+                    execute_batch_build=False
+        )
+
+        aws_codepipeline.Pipeline(
+            self, "Sentinel",
+            pipeline_name=f"sentinel",
+            stages=[{
+                "stageName": "Source",
+                "actions": [source_action]
+            }, {
+                "stageName": "Build",
+                "actions": [build_action]
+            }
+            ]
+        )
+        """
+        pipeline = aws_codepipeline.Pipeline(
+            self, "Sentinel",
+            pipeline_name=f"sentinel",
+            stages=[
+                aws_codepipeline.StageProps(
+                    stage_name='Source',
+                    actions=[
+                        aws_codepipeline_actions.GitHubSourceAction(
+                            action_name="GitHub_Source",
+                            owner="alenkrmelj",
+                            repo="sentinel_demo",
+                            output=source_output,
+                            branch="main",
+                            oauth_token=SecretValue.secrets_manager("github-oauth-token"),
+                        ),
+                    ]
+                ),
+                build_action = codepipeline_actions.CodeBuildAction(
+                    action_name="CodeBuild",
+                    project=codebuild_project,
+                    input=source_output,
+                    outputs=[codepipeline.Artifact("imagedefinitions")],
+                    execute_batch_build=False
+        )
+            ]
+        )
+        aws_codepipeline.StageProps(
+            stage_name='Build',
+            actions=[
+                aws_codepipeline_actions.CodeBuildAction(
+                    action_name='DockerBuildImages',
+                    input=source_output,
+                    project=docker_build,
+                )
+            ]
+        )
+        """
